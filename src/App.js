be83fb0d1b11c96a0d72edb64a31032973703ca3
Al West <al@tsew.net>
2023-10-12 12:44:06 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [length, setLength] = useState(12);
+  const [useLetters, setUseLetters] = useState(true);
+  const [useNumbers, setUseNumbers] = useState(true);
+  const [useSymbols, setUseSymbols] = useState(true);
+  const [password, setPassword] = useState('');
+
+  const generatePassword = () => {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_-+=<>?';
+
+    let characters = '';
+    if (useLetters) characters += letters;
+    if (useNumbers) characters += numbers;
+    if (useSymbols) characters += symbols;
+
+    let generatedPassword = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      generatedPassword += characters[randomIndex];
+    }
+
+    setPassword(generatedPassword);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Password Generator</h2>
+      <div>
+        <label>
+          Length: 
+          <input 
+            type="number" 
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Letters:
+          <input 
+            type="checkbox" 
+            checked={useLetters}
+            onChange={() => setUseLetters(!useLetters)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Numbers:
+          <input 
+            type="checkbox" 
+            checked={useNumbers}
+            onChange={() => setUseNumbers(!useNumbers)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Symbols:
+          <input 
+            type="checkbox" 
+            checked={useSymbols}
+            onChange={() => setUseSymbols(!useSymbols)}
+          />
+        </label>
+      </div>
+      <button onClick={generatePassword}>Generate</button>
+      <hr/>
+      <div>
+        <strong>Password:</strong> {password}
+      </div>
     </div>
   );
 }
 
 export default App;
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { waitFor } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+describe('<App />', () => {
+
+  test('renders without crashing', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Password Generator/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('defaults to 12 characters', () => {
+    render(<App />);
+    const inputElement = screen.getByLabelText(/Length:/i);
+    expect(inputElement.value).toBe('12');
+  });
+
+  test('generates a password', () => {
+    render(<App />);
+    const generateButton = screen.getByText(/Generate/i);
+    fireEvent.click(generateButton);
+    const passwordText = screen.getByText(/Password:/i);
+    expect(passwordText).toBeInTheDocument();
+  });
+
+  test('handles letters checkbox toggling', () => {
+    render(<App />);
+    const lettersCheckbox = screen.getByLabelText(/Letters:/i);
+    expect(lettersCheckbox.checked).toBe(true);
+    fireEvent.click(lettersCheckbox);
+    expect(lettersCheckbox.checked).toBe(false);
+  });
+
+  test('handles numbers checkbox toggling', () => {
+    render(<App />);
+    const numbersCheckbox = screen.getByLabelText(/Numbers:/i);
+    expect(numbersCheckbox.checked).toBe(true);
+    fireEvent.click(numbersCheckbox);
+    expect(numbersCheckbox.checked).toBe(false);
+  });
+
+  test('handles symbols checkbox toggling', () => {
+    render(<App />);
+    const symbolsCheckbox = screen.getByLabelText(/Symbols:/i);
+    expect(symbolsCheckbox.checked).toBe(true);
+    fireEvent.click(symbolsCheckbox);
+    expect(symbolsCheckbox.checked).toBe(false);
+  });
+
+
+
 });
+
